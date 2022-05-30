22c8d012bc629b2d65a422c2d1cdd7fbba748dc0
fselvino <fernandoselvino@gmail.com>
2022-05-30 07:05:00 -0300

    Pagamento no Stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,16 +1,46 @@
+import { useSession, signIn } from 'next-auth/react'
+import { api } from '../../services/api'
+import { getStripeJs } from '../../services/stripe-js'
 import styles from './styles.module.scss'
 
 interface SubscribeButtonProps {
     priceId: string
 }
 
+//locais seguros para fazer transaçoes com chave privada
+//getServerSideProps (SSR)
+//getStaticProps (SSR)
+// API routes
+
 
 export function SubscribeButton({priceId}: SubscribeButtonProps){
 
+    const {data: session} = useSession() 
+
+    async function handleSubscribe(){
+        if(!session){
+            signIn()
+            return
+        }
+
+        try {
+            const response = await api.post('/subscribe')
+
+            const {sessionId } = response.data
+          
+            const stripe = await getStripeJs()
+
+            await stripe.redirectToCheckout({sessionId})
+
+        }catch (err){
+            alert(err.message)
+        }
+    }
    
     return (
         <button type="button"
         className={styles.subscribeButton}
+        onClick={handleSubscribe}
         >
             Subscribe Now
         </button>
